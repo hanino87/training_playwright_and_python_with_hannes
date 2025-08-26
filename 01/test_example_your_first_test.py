00b039c325fecffbc39bd79aef3a7fd944f084ad
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-08-26 22:40:09 +0200

    Update test_example_your_first_test.py

--- 01/test_example_your_first_test.py
+++ 01/test_example_your_first_test.py
@@ -1,9 +1,6 @@
-´´´ Write a test with one expect choose your website and try to do some metods from page Class Library and use 
-    expect library for implementing end-to-end frontend testing. 
-
-    For expect metods see https://playwright.dev/python/docs/test-assertions
-
-    For page metods 
+´´´Write a test with one expect choose your website and try to do some metods from page Class Library and use 
+   expect library for implementing end-to-end frontend testing. 
+    
 ´´´
 
 
@@ -12,5 +9,7 @@
 def test_homepage_url(page):
 page.goto("https://www.svt.se")
 
-expect(page).to_have_url("https://www.svt.se/") 
+expect(page).to_have_url("https://www.svt.se/") # assert webpage has right url for user 
+
+expect(response).to_be_ok() # assert repsonse from webiste is okay 
 
