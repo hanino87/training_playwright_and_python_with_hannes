12f22b6f97d1de6efa204cc512566a7f3217a00c
Hannes Uhlin Collmo <148004074+hanino87@users.noreply.github.com>
2025-08-26 22:34:54 +0200

    Update test_example_your_first_test.py

--- 01/test_example_your_first_test.py
+++ 01/test_example_your_first_test.py
@@ -1,3 +1,10 @@
 ´´´ Write a test with one expect choose your website and try to do some metods from page Class Library and use 
-    expect for do test assertion validation 
+    expect library for implementing end-to-end frontend testing. 
+
+    For expect metods see https://playwright.dev/python/docs/test-assertions
+
+    For page metods 
 ´´´
+
+
+### Test below 
